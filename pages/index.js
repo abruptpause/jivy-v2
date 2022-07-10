93685cf4f15bc1bb3bd5180433943e9386faa586
ixc7 <isaacxpreston7@gmail.com>
2022-07-10 12:33:20 -0400

    remove Layout wrapper component

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,14 @@ import { createClient } from "../prismicio"
 import { components } from "../slices/"
 import { Layout } from "../components/Layout"
 
-const Index = ({ page, navigation, settings }) => {
-  return (
-    <Layout navigation={navigation} settings={settings}>
+const Index = ({ page }) => (
+    <>
       <Head>
         <title>{prismicH.asText(page.data.title)}</title>
       </Head>
       <SliceZone slices={page.data.slices} components={components} />
-    </Layout>
+    </>
   )
-}
 
 export default Index
 

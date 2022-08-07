81703762ce5b2dd9eb5c19969545d570534ab551
ixc7 <isaacxpreston7@gmail.com>
2022-08-06 23:41:47 -0400

    add states for expanded/collapsed hero and featured sections

--- components/components.js
+++ components/components.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-export const Section = ({ children, bg = 'bg-slate-100', width }) => (
+export const Section = ({ children, bg = 'bg-slate-100', width = 'col-span-10 col-start-2' }) => (
   <div className={`min-h-screen grid grid-cols-12 ${bg}`}>
     <div className={`${width} grid grid-cols-12 gap-y-14 m-14`}>{children}</div>
   </div>
@@ -26,8 +26,7 @@ export const Work = ({ src, className }) => (
 
 const Square = ({ src, start }) => (
   <div className={`
-    relative grid justify-center w-full items-center
-    aspect-[1/1] bg-white my-8 scroll-element col-span-7 ${start}
+    relative aspect-[1/1] my-8 scroll-element col-span-7 ${start}
   `}>
     <Image layout='fill' alt='' src={src} />
   </div>
@@ -37,34 +36,43 @@ export const Featured = ({page}) => {
   const featured = page.data.slices.filter(slice => slice.slice_type === 'featured')[0]
 
   return (
-    <div className='absolute top-0 left-0 grid grid-cols-3 w-full h-full z-10'>
+    <div
+      className='grid grid-cols-3 absolute top-0 left-0 w-full h-full z-10'
+      // style={{transform: 'translateX(10%)'}}
+    >
       <div className={`
-        sticky overflow-y-scroll col-span-1 col-start-3 h-screen top-0
-        scroll-container bg-slate-300 grid grid-cols-12 scroll-py-20
+        grid grid-cols-12
+        col-span-1 col-start-3
+        sticky overflow-y-scroll h-screen top-0
+        scroll-container bg-slate-300 scroll-py-20
       `}>
         <h1
           className='col-span-12 text-white freight-neo text-center top-0 left-0 text-3xl pt-6 mb-8 sticky'
         >
           {featured.primary.title}
         </h1>
-        {featured.items.map((i, k) => <Square key={k} src={i.image.url} start={k % 2 === 0 ? 'col-start-2' : 'col-start-5'} />)}
+        {featured.items.map((i, k) => (
+          <Square key={k} src={i.image.url} start={k % 2 === 0 ? 'col-start-2' : 'col-start-5'} />
+        ))}
       </div>
     </div>
   )
 }
 
-export const Header = ({page}) => (
+export const Hero = ({page, width, index}) => (
   <div
-    className='absolute top-0 left-0 grid grid-cols-3 w-full z-10'
+    className={`grid grid-cols-3 w-full absolute top-0 left-0 ${index}`}
     style={{ height: '200vh' }}
   >
     <div className={`
-      freight-neo sticky flex h-screen top-0 text-center text-white
-      text-9xl col-span-2 justify-center items-center
+      ${width}
+      h-screen top-0 sticky
+      freight-neo text-center text-white
+      flex justify-center items-center
     `}>
       <div>
         <h1 className='text-xl pb-12'>{page.data.subtitle[0].text}</h1>
-        {page.data.title.map((i, k) => <h1 key={k}>{i.text}</h1>)}
+        {page.data.title.map((i, k) => <h1 key={k} className='text-9xl'>{i.text}</h1>)}
       </div>
     </div>
   </div>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // https://jivy-v2.vercel.app/
 import { useState } from 'react'
 import { createClient } from "../prismicio"
-import { Featured, Header, Section, Text, Work } from '../components/components.js'
+import { Featured, Hero, Section, Text, Work } from '../components/components.js'
 
 const Home = ({ page }) => {
   const { about, image, slices } = page.data
@@ -13,48 +13,51 @@ const Home = ({ page }) => {
 
   const heroImage = { backgroundImage: `url('${image.url}')` }
 
-  const [width, setWidth] = useState('col-span-10 col-start-2')
-  // 'col-span-8 col-start-1'
+  // col-span-2 or col-span-3 when expanded
+  const [width, setWidth] = useState('col-span-2')
+
+  // blank or z-10 when expanded
+  const [index, setIndex] = useState('')
 
   return (
     <main>
       <div className='relative grid grid-cols-3'>
-        <Header page={page} />
         <Featured page={page} />
 
-        <div className='col-span-2 relative h-screen bg-center bg-cover text-slate-200' style={heroImage} />
-        <div className='col-span-2 relative flex h-screen bg-slate-200 items-end justify-center' />
+        <div className={`${width} ${index} h-screen bg-center bg-cover text-slate-200`} style={heroImage} />
+        <div className={`${width} ${index} h-screen bg-slate-200`} />
+        <Hero page={page} width={width} index={index} />
 
-        <div className='col-span-2 relative bg-slate-200 text-justify grid grid-cols-5'>
-          <div className='pb-24 col-span-3 col-start-2'>
+        <div className={`${width} ${index} grid grid-cols-5 col-span-2 bg-slate-200 text-justify`}>
+          <div className='col-span-3 col-start-2 mb-24'>
             {about.map((i, k) => (
-              <p key={k} className='font-extralight mb-4 text-slate-500 text-xl'>
+              <p key={k} className='text-xl text-slate-500 font-extralight mb-4'>
                 {i.text}
               </p>
             ))}
           </div>
         </div>
+
       </div>
 
-      <Section width={width}>
+      <Section>
         <Work src={art[0].image} className='col-span-8 aspect-[5/7] self-start' />
         <Text item={art[0]} className='col-span-4 self-end' />
         <Text item={art[1]} className='col-span-8 self-end text-right' />
         <Work src={art[1].image} className='col-span-4 aspect-[4/5] self-end mb-24' />
       </Section>
 
-      <Section bg='bg-slate-200' width={width}>
+      <Section bg='bg-slate-200'>
         <Work src={art[2].image} className='col-span-6 aspect-[2/3] self-start' />
         <Text item={art[2]} className='col-span-6 self-end' />
       </Section>
 
-      <Section bg='bg-slate-400' width={width}>
-        {/* <Work src={art[3].image} className='col-span-8 col-start-3 aspect-[7/5] bg-slate-500 self-center' /> */}
-        <Work src={art[3].image} className='col-span-12 aspect-[7/5] bg-slate-500 self-center' />
-        <Text item={art[3]} className='col-span-8 col-start-3 text-center' primary='text-slate-600' secondary='text-slate-700' />
+      <Section bg='bg-slate-400'>
+        <Work src={art[3].image} className='col-span-10 col-start-2 aspect-[7/5] bg-slate-500 self-center' />
+        <Text item={art[3]} className='col-span-10 col-start-2 text-center' primary='text-slate-600' secondary='text-slate-700' />
       </Section>
 
-      <Section width={width}>
+      <Section>
         <Work src={art[4].image} className='col-span-6 aspect-[4/5] self-start' />
         <Text item={art[4]} className='col-span-5 self-end' />
         <Text item={art[5]} className='col-span-6 self-end text-right' />

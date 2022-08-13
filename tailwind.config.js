1fdfa49a1980284a757db1faed8a46b8dc96e65d
ixc7 <isaacxpreston7@gmail.com>
2022-08-13 16:38:05 -0400

    remove tailwind.config.js aspect ratios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,3 @@
 module.exports = {
   content: ["./pages/**/*.js", "./components/**/*.js", "./slices/**/*.js"],
-  theme: {
-    extend: {
-      // https://tailwindcss.com/docs/aspect-ratio#using-custom-values
-      aspectRatio: {
-        '2/3': '2 / 3',
-        '4/5': '4 / 5',
-        '5/7': '5 / 7',
-        '4/3': '4 / 3',
-        '7/5': '7 / 5'
-      }
-    }
-  },
-  plugins: [require("@tailwindcss/aspect-ratio")],
 };
